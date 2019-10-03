f3e64ae48c2e5bd38c89688ba1f5b432b76e2a71
Icesiolz <Icesiolz@users.noreply.github.com>
2019-10-03 14:14:32 +0800

    Revert "Removed Address class using manual refactoring"
    This reverts commit bb7eaed82d7d35b348f20440e5e893d9bc1fb631.

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -9,7 +9,10 @@ import java.util.Set;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.*;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -75,17 +78,6 @@ public class ParserUtil {
             throw new ParseException(Address.MESSAGE_CONSTRAINTS);
         }
         return new Address(trimmedAddress);
-    }
-
-    /**
-     * Parses a {@code String remark} into an {@code Remark}.
-     * Leading and trailing whitespaces will be trimmed.
-     *
-     */
-    public static Remark parseRemark(String remark) {
-        requireNonNull(remark);
-        String trimmedAddress = remark.trim();
-        return new Remark(trimmedAddress);
     }
 
     /**
